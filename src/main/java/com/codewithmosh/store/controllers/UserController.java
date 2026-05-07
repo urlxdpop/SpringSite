ed908afca02525c702514acd312dc5172d34697f
urlxd <nikita.qwerty.trtio@gmail.com>
2026-05-07 14:24:01 +0600

    Add Mapper

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.codewithmosh.store.controllers;
 
+import java.util.Set;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codewithmosh.store.DTOs.UserDto;
-import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.mappers.UserMapper;
 import com.codewithmosh.store.repositories.UserRepository;
 
 import lombok.AllArgsConstructor;
@@ -17,20 +21,28 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @GetMapping
-    public Iterable<UserDto> getAllUsers(){
-        return userRepository.findAll()
+    public Iterable<UserDto> getAllUsers(
+        @RequestParam(required = false, defaultValue = "", name = "sort") String sort
+    ){
+        if (!Set.of("name", "email").contains(sort))
+            sort = "name";
+
+        return userRepository.findAll(Sort.by(sort))
         .stream()
-        .map(user -> new UserDto(user.getId(), user.getName(), user.getEmail()))
+        .map(userMapper::toDto)
         .toList();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserDto> getUserById(@PathVariable Long id){
-        return userRepository.findById(id)
-        .map(user -> new UserDto(user.getId(), user.getName(), user.getEmail()))
-        .map(ResponseEntity::ok)
-        .orElse(ResponseEntity.notFound().build());
+        var user = userRepository.findById(id);
+
+        if (user == null)
+            return ResponseEntity.notFound().build();
+        
+        return ResponseEntity.ok(userMapper.toDto(user.get()));
     }
 }
--- src/main/java/com/codewithmosh/store/entities/User.java
+++ src/main/java/com/codewithmosh/store/entities/User.java
@@ -60,9 +60,6 @@ public class User {
     @Builder.Default
     private Set<Tag> tags = new HashSet<>();
 
-    @OneToOne(mappedBy = "user")
-    private Profile profile;
-
     @ManyToMany
     @JoinTable(
         name = "wishlist",
